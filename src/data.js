ba9ba1fa75c74363ed8b897e15b3cce6a140c2fe
Gayan <you@example.comomalkagayan@gmail.com>
2022-11-16 18:50:34 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,66 @@
+const data = [
+  {
+    question: "What is a state in React?",
+    incorrectAnswers: [
+      "A permanent storage.",
+      "External storage of the component.",
+      "None of the above.",
+    ],
+    correctAnswer: "Internal storage of the component.",
+  },
+
+  {
+    question:
+      "What are the two ways to handle data in React?",
+    incorrectAnswers: [
+      "Services & Components",
+      "State & Services",
+      "State & Component",
+    ],
+    correctAnswer: "State & Props",
+  },
+  {
+    question: "What is the default port where webpack-server runs?",
+    incorrectAnswers: ["3000", "3030", "6060"],
+    correctAnswer: "8080",
+  },
+  {
+    question: "In which of the following directory React.js components are saved?",
+    incorrectAnswers: ["Inside the vendor/components/", "Inside the external/components/", "Inside the vendor/"],
+    correctAnswer: "Inside the js/components/",
+  },
+  {
+    question: "What is the use of the create-react-app command in the React.js application?",
+    incorrectAnswers: [
+      "It is used to update a React app.",
+      "It is used to install dependencies.",
+      "None of the above.",
+    ],
+    correctAnswer: "It is used to create a new React app.",
+  },
+  {
+    question: "Which of the following method refers to the parent class in React.js?",
+    incorrectAnswers: [
+      "inherits()",
+      "self()",
+      "this()",
+    ],
+    correctAnswer: "super()",
+  },
+  {
+    question: "Which of the following is used to pass data to a component from outside in React.js?",
+    incorrectAnswers: ["SetState", "Render with arguments", "PropTypes"],
+    correctAnswer: "Props",
+  },
+  {
+    question: "Does React.js create a VIRTUAL DOM in the memory?",
+    incorrectAnswers: [
+      "FALSE",
+      "Can be true or false",
+      "Cannot say",
+    ],
+    correctAnswer: "TRUE",
+  },
+];
+
+export default data;
